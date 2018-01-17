5b8a1a3ca55eff9392f19a51d7a99886509d105f
Michael Stifter <stifter.michael@gmx.net>
2018-01-17 22:35:07 +0100

    Add sample implementation of Vuex store

---
+++ src/store/modules/products.js
@@ -0,0 +1,38 @@
+import * as types from '../mutation-types'
+
+// initial state
+const state = {
+  all: []
+}
+
+// getters
+const getters = {
+  allProducts: state => state.all
+}
+
+// actions
+const actions = {
+  getAllProducts ({ commit }) {
+    // TODO get products from API
+    commit(types.RECEIVE_PRODUCTS, { products: [] })
+  }
+}
+
+// mutations
+const mutations = {
+  [types.RECEIVE_PRODUCTS] (state, { products }) {
+    state.all = products
+  },
+
+  [types.ADD_TO_CART] (state, { id }) {
+    state.all.find(product => product.id === id).inventory--
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
+
