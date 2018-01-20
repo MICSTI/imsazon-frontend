16dd21d91c2abc8147e96bd682a48787be7f6cbd
Michael Stifter <stifter.michael@gmx.net>
2018-01-20 09:15:25 +0100

    Add store modules to store's index file

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as actions from './actions'
+import cart from './modules/cart'
+import products from './modules/products'
 
 Vue.use(Vuex)
 
@@ -9,7 +11,8 @@ const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
   actions,
   modules: {
-
+    cart,
+    products
   },
   strict: debug
 })
