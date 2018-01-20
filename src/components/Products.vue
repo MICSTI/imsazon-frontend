a4ca9053ddbeb453dbad5ef0bf54bb1712f415d8
Michael Stifter <stifter.michael@gmx.net>
2018-01-20 20:51:18 +0100

    Extend basic product list item display

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,13 +1,27 @@
 <template>
     <ul>
       <li v-for="product in products">
-        {{ product.Name }} - {{ product.Price | currency }}
-        <br/>
-        <button
-          :disabled="!product.Quantity"
-          @click="addToCart(product)">
-          Add to cart
-        </button>
+        <div class="product-image-container">
+          <img :src="product.ImageUrl" width="56" height="56" />
+        </div>
+
+        <div class="product-detail">
+          <div class="product-name">{{ product.Name }}</div>
+          <div class="product-description">{{ product.Description }}</div>
+        </div>
+
+        <div class="product-price">
+          <div class="product-price-display">{{ product.Price | currency }}</div>
+          <div class="product-quantity"><span v-bind:class="{ red: product.Quantity <= 3 }"><span v-if="product.Quantity <= 3">only </span>{{product.Quantity}} available</span></div>
+        </div>
+
+        <div class="product-add">
+          <button
+            :disabled="!product.Quantity"
+            @click="addToCart(product)">
+            Add
+          </button>
+        </div>
       </li>
     </ul>
 </template>
@@ -30,5 +44,52 @@
 </script>
 
 <style scoped>
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
 
+  li  {
+    border: 1px solid #ecf0f1;
+    padding: .7em .4em;
+    margin-bottom: 0.7em;
+    display: flex;
+    align-items: center;
+  }
+
+  .red {
+    color: red;
+  }
+
+  .product-image-container {
+    flex: 1 0;
+  }
+
+  .product-detail {
+    flex: 4 0;
+  }
+
+  .product-price {
+    flex: 2 0;
+  }
+
+  .product-add {
+    flex: 1 0;
+  }
+
+  .product-name {
+    font-weight: bold;
+    font-size: 1.5em;
+    margin-bottom: .5em;
+  }
+
+  .product-description {
+    font-size: .8em;
+  }
+
+  .product-quantity {
+    font-size: .8em;
+    margin-top: .3em;
+  }
 </style>
