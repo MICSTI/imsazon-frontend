9cdb70b7223f77900614adbc0dfbc062a3e85146
Michael Stifter <stifter.michael@gmx.net>
2018-01-25 21:06:29 +0100

    Add basic login component

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+  </form>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
