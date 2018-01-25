1d1c64c648986c246824ed8cb4b62583bf0f5d6f
Michael Stifter <stifter.michael@gmx.net>
2018-01-25 21:24:19 +0100

    Add style to login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,21 @@
 <template>
   <form>
-    <div class="form-group">
-      <label for="username">Username:</label>
-      <input type="text" id="username" v-model="username" />
-    </div>
+    <div class="form-header">Sign in to IMSazon</div>
 
-    <div class="form-group">
-      <label for="password">Password:</label>
-      <input type="password" id="password" v-model="password" />
+    <div class="form-inner">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+
+      <div class="form-group">
+        <input type="button" class="btn-signin" value="Sign in" />
+      </div>
     </div>
   </form>
 </template>
@@ -17,5 +25,47 @@
 </script>
 
 <style scoped>
+  form {
+    width: 50%;
+    background-color: #d2d2d2;
+    padding: 1.2em .6em;
+    margin-top: 2em;
+    margin-left: 25%;
+  }
 
+  .form-header {
+    font-weight: bold;
+    font-size: 1.35em;
+    margin-bottom: 1.5em;
+  }
+
+  .form-inner {
+    width: 100%;
+    margin-left: 25%;
+  }
+
+  .form-group {
+    margin-bottom: 1.5em;
+    text-align: left;
+  }
+
+  .form-group > label, .form-group > input {
+    display: block;
+  }
+
+  .btn-signin {
+    font-size: 1.2em;
+  }
+
+  label {
+    color: #444;
+    text-transform: uppercase;
+    font-size: .95em;
+    margin-bottom: .25em;
+  }
+
+  input {
+    width: 50%;
+    height: 2em;
+  }
 </style>
