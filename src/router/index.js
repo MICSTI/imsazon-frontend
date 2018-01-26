29d6cdcc08965c1d96a912f9ed1fac1d6174eea4
Michael Stifter <stifter.michael@gmx.net>
2018-01-26 11:19:16 +0100

    Add beforeEach hook to router to check for login

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Login from '@/components/Login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/login',
@@ -35,3 +35,10 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // TODO check if user is logged in
+  next()
+})
+
+export default router
