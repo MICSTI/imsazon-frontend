13abcfa2f137988ab1b497e1906dbb794da5bdf5
Michael Stifter <stifter.michael@gmx.net>
2018-01-30 11:06:02 +0100

    Add mock credit card verifying display

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -6,13 +6,22 @@
         <button @click="buy">Buy</button>
       </div>
       <div class="payment-status">
-
+        <div class="payment-status-item payment-status-verifying">
+          <div class="payment-status-item-header">Verifying credit card</div>
+          <div>Using credit card ending in <span class="bold brand">7890</span>.</div>
+          <div><img src="../assets/spinner.gif" width="160" height="120" /></div>
+        </div>
       </div>
     </div>
 </template>
 
 <script>
   export default {
+    data: () => {
+      return {
+
+      }
+    },
     methods: {
       buy () {
         console.log('buying now')
@@ -35,6 +44,21 @@
     font-weight: bold;
     padding-bottom: .5em;
     border-bottom: 1px solid #ccc;
+    margin-bottom: 1.5em;
+  }
+
+  .payment-status-item {
+    border: 3px solid #aaa;
+    padding: 2em;
+    margin: 3em;
+  }
+
+  .payment-status-item-header {
+    text-transform: uppercase;
+    font-size: 1.1em;
+    text-align: left;
+    padding-bottom: .3em;
+    border-bottom: 1px solid #777;
     margin-bottom: 1.5em;
   }
 </style>
